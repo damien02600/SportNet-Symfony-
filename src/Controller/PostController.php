51e101d1923ee20d0aa315f6ebed449e4159c7fe
damien berthe <d.berthe1997@laposte.net>
2023-04-21 11:05:53 +0200

    commentaire postController

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,6 +1,6 @@
 <?php
 
-// Je créer un HomeController qui sera le controller de ma page home.html.twig, ce sera ma page d'accueil de mon site
+// Je créer un PostController qui sera le controller de mes post
 namespace App\Controller;
 
 use App\Repository\PostRepository;
@@ -9,16 +9,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /* 
-Je créer une class HomeController qui a le meme nom que mon controller, 
+Je créer une class PostController qui a le meme nom que mon controller, 
 cette class étends AbstractController qui elle contient la méthode render.
-La méthode render permet d'afficher la vue dans la page home.html.twig.
+La méthode render permet d'afficher la vue dans les pages
 */
 
 class PostController extends AbstractController
 {
     /* 
     ROUTE permet de diriger sur quelle page le controller fera effets.
-    On lui mets un path (un chemin) et un name (home.index).
+    On lui mets un path (un chemin) et un name (post.index).
     */
     #[Route("/post", "post.index")]
 
@@ -26,16 +26,23 @@ class PostController extends AbstractController
     Je créer une fonction index qui me retourne une Response qui vient du HttpFoundation du composant Symfony.
     Une Response est un objet qui envoie les info à partir d'une demande.
     */
+
+    // Ce controller sert à afficher les annonces des utilisateurs.
+
+    /* 
+    J'utilise le repository, le repository va récupérer toute la récupération de données.
+    Donc du-coup j'importe le repository (PostRepository) que je renomme $repository
+    */
     public function index(PostRepository $repository): Response
     {
         /* 
         Je lui retourne la méthode render.
-        La méthode render permet d'afficher la vue dans la page home.html.twig.
+        La méthode render permet d'afficher la vue dans la page post.html.twig.
         */
         return $this->render('pages/post/post.html.twig', [
             /**
-             * Je dit que ingrédients est égal, j'appelle le repository est je fais un findAll.
-             * FindAll permet de récuperer tout les données dans la base de donnée
+             * Je lui passe à la vue une variable 'post, j'appelle le repository est je fais un findAll.
+             * FindAll permet de récuperer tout les données dans la base de donnée.
              */
             'post' => $repository->findAll()
         ]);
