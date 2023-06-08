fdfba1d637b6f3876cf2c38b1f3e09d5681b79ba
damien berthe <d.berthe1997@laposte.net>
2023-06-08 10:24:56 +0200

    CrudControllerPost avec EasyAdmin

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -1,9 +1,15 @@
 <?php
-
+// J'utilise la commande php bin/console make:admin:crud 
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class PostCrudController extends AbstractCrudController
 {
@@ -12,14 +18,27 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    /*
+public function configureCrud(Crud $crud): Crud
+{
+    return $crud
+    ->setEntityLabelInPlural('Annonces')
+    ->setEntityLabelInSingular('Annonce')
+    ->setPageTitle("index","Sportnet - Administration des annonces")
+    ->setPaginatorPageSize(10);
+}
+
+
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')
+            ->hideOnForm(),
             TextField::new('title'),
             TextEditorField::new('description'),
+            DateTimeField::new('createdAt')
+            ->setFormTypeOption('disabled', 'disabled'),
         ];
     }
-    */
+    
 }
